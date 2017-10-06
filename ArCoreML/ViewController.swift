aed14512f11a08ceab0201e9b4edc7aa900c2f48
Hsanka6 <haasith2@gmail.com>
2017-10-06 11:38:24 -0700

    Created an that recognizes objects in AR and displays a label on top of them

--- ArCoreML/ViewController.swift
+++ ArCoreML/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 import SceneKit
 import ARKit
+import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    private var resnetModel = Resnet50()
+    
+    
+    private var hitTestResult : ARHitTestResult!
+    private var visionRequests = [VNRequest]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,10 +30,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        registerGestureRecognizers()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -62,6 +69,156 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    
+    
+    private func registerGestureRecognizers() {
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer: UITapGestureRecognizer)
+    {
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = self.sceneView.center
+        
+        guard let currentFrame = sceneView.session.currentFrame else {
+            return
+        }
+        
+        let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
+        
+        
+        if hitTestResults.isEmpty {
+            return
+        }
+        
+        guard let hitTestResult = hitTestResults.first else
+        {
+            return
+        }
+        self.hitTestResult = hitTestResult
+        
+        let pixelBuffer = currentFrame.capturedImage
+        
+        
+        performVisionRequest(pixelBuffer: pixelBuffer)
+    }
+    
+    private func performVisionRequest(pixelBuffer:CVPixelBuffer) {
+        let visionModel = try!VNCoreMLModel(for: resnetModel.model)
+        
+        let request = VNCoreMLRequest(model: visionModel) { request, error in
+            if error != nil{
+                print(error.debugDescription)
+                return
+            }
+            
+            guard let observations = request.results else {
+                return
+            }
+            let observation = observations.first as! VNClassificationObservation
+            
+            
+            print("Name \(observation.identifier) and confidence is \(observation.confidence)")
+            
+            
+            DispatchQueue.main.async {
+                self.displayPredictions(text:observation.identifier)
+            }
+        }
+        
+        request.imageCropAndScaleOption = .centerCrop
+        
+        self.visionRequests = [request]
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .upMirrored, options: [ : ])
+        
+        DispatchQueue.global().async {
+            try! imageRequestHandler.perform(self.visionRequests)
+        }
+    }
+    
+    
+    private func displayPredictions(text: String)
+    {
+       //let node = createText(text: text)
+        let node = createNewBubbleParentNode ( text )
+        node.position = SCNVector3(self.hitTestResult.worldTransform.columns.3.x,self.hitTestResult.worldTransform.columns.3.y, self.hitTestResult.worldTransform.columns.3.z)
+        
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+    }
+    
+    func createNewBubbleParentNode ( _ text : String ) -> SCNNode {
+        
+        let bubbleDepth: Float = 0.01
+         // TEXT BILLBOARD CONSTRAINT
+         let billboardConstraint = SCNBillboardConstraint ()
+         billboardConstraint.freeAxes = SCNBillboardAxis.Y
+        // BUBBLE-TEXT
+         let bubble = SCNText ( string : text , extrusionDepth :
+            CGFloat ( bubbleDepth ))
+         var font = UIFont ( name : "Futura" , size : 0.15 )
+         //font = font?.withTraits(traits: .traitBold)
+         bubble.font = font
+        bubble.alignmentMode = kCAAlignmentCenter
+        bubble.firstMaterial?.diffuse.contents = UIColor.orange
+        bubble.firstMaterial?.specular.contents = UIColor.white
+        bubble.firstMaterial?.isDoubleSided = true
+         // bubble.flatness // setting this too low can causecrashes.
+         bubble.chamferRadius = CGFloat ( bubbleDepth )
+        
+         // BUBBLE NODE
+         let ( minBound , maxBound ) = bubble.boundingBox
+         let bubbleNode = SCNNode ( geometry : bubble )
+         // Centre Node - to Centre-Bottom point
+        bubbleNode.pivot = SCNMatrix4MakeTranslation ((maxBound.x - minBound.x)/2 , minBound.y, bubbleDepth/2 )
+         // Reduce default text size
+        bubbleNode.scale = SCNVector3Make ( 0.2 , 0.2 , 0.2)
+        // CENTRE POINT NODE
+         let sphere = SCNSphere ( radius : 0.005 )
+         sphere.firstMaterial?.diffuse.contents = UIColor.cyan
+         let sphereNode = SCNNode ( geometry : sphere )
+        
+         // BUBBLE PARENT NODE
+         let bubbleNodeParent = SCNNode ()
+         bubbleNodeParent.addChildNode ( bubbleNode )
+         bubbleNodeParent.addChildNode ( sphereNode )
+         bubbleNodeParent.constraints = [ billboardConstraint ]
+        
+         return bubbleNodeParent
+        
+    }
+    
+    
+    
+    private func createText(text: String)->SCNNode{
+        let parentNode = SCNNode()
+        
+        let sphere = SCNSphere(radius: 0.01)
+        
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.diffuse.contents = UIColor.orange
+        sphere.firstMaterial = sphereMaterial
+        let sphereNode =  SCNNode(geometry: sphere)
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 0)
+        textGeometry.alignmentMode = kCAAlignmentCenter
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.orange
+        textGeometry.firstMaterial?.specular.contents = UIColor.white
+        textGeometry.firstMaterial?.isDoubleSided = true
+        
+        let font = UIFont(name: "Futura", size: 0.15)
+        
+        textGeometry.font = font
+        let textNode = SCNNode(geometry: textGeometry)
+        textNode.scale = SCNVector3Make(0.2, 0.2, 0.2)
+        
+        parentNode.addChildNode(sphereNode)
+        parentNode.addChildNode(textNode)
+        return parentNode
+    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
